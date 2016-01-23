a8f770f47f4737ce5b304d772f89a321c0eef349
Willi Ballenthin <willi.ballenthin@gmail.com>
2016-01-23 19:05:36 +0000

    add license, setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ida-settings',
+      version="0.1",
+      description="Fetch and set configuration values in IDA Pro IDAPython scripts",
+      author='Willi Ballenthin',
+      author_email='william.ballenthin@fireeye.com',
+      url='https://github.com/williballenthin/ida-settings',
+      license='Apache License (2.0)',
+      packages=['ida_settings'],
+      classifiers = ["Programming Language :: Python",
+                     "Programming Language :: Python :: 2",
+                     "Operating System :: OS Independent",
+                     "License :: OSI Approved :: Apache Software License"],
+     )
