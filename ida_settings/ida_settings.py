11f6c063f4ec1fded3270cff9c911e0392dc73a4
Willi Ballenthin <wilbal1087@gmail.com>
2016-02-03 22:05:01 -0500

    update doc to single constructor arg

--- ida_settings/ida_settings.py
+++ ida_settings/ida_settings.py
@@ -1,26 +1,26 @@
 """
 ida_settings provides a mechanism for settings and fetching
 configration values for IDAPython scripts and plugins.
-Configurations are namespaced by author organization and
-plugin name, and scoped to the global system, current user,
+Configurations are namespaced by plugin name,
+and scoped to the global system, current user,
 working directory, or current IDB file. Configurations
 can be exported and imported using an .ini-style intermediate
 representation.
 
 Example fetching a configuration value:
 
-    settings = IDASettings("com.fireeye.flare", "MSDN-doc")
+    settings = IDASettings("MSDN-doc")
     if settings["verbosity"] == "high":
         ...
 
 Example setting a global configuration value:
 
-    settings = IDASettings("com.fireeye.flare", "MSDN-doc")
+    settings = IDASettings("MSDN-doc")
     settings.system["verbosity"] = "high"
 
 Example setting a working directory configuration value:
 
-    settings = IDASettings("com.fireeye.flare", "MSDN-doc")
+    settings = IDASettings("MSDN-doc")
     settings.directory["verbosity"] = "high"
 
 Use the properties "system", "user", "directory" and "idb"
@@ -29,7 +29,7 @@ system, current user, working directory, or current IDB file.
 
 Treat a settings instance like a dictionary. For example:
 
-    settings = IDASettings("com.fireeye.flare", "MSDN-doc")
+    settings = IDASettings("MSDN-doc")
     "verbosity" in settings --> False
     settings["verbosity"] = "high"
     settings["verbosity"] --> "high"
@@ -40,13 +40,13 @@ Treat a settings instance like a dictionary. For example:
 To export the current effective settings, use the `export_settings`
 function. For example:
 
-    settings = IDASettings("com.fireeye.flare", "MSDN-doc")
+    settings = IDASettings("MSDN-doc")
     export_settings(settings, "/home/user/desktop/current.ini")
 
 To import existing settings into a settings instance, such as
 the open IDB, use the `import_settings` function. For example:
 
-    settings = IDASettings("com.fireeye.flare", "MSDN-doc")
+    settings = IDASettings("MSDN-doc")
     import_settings(settings.idb, "/home/user/desktop/current.ini")
 
 This module is a single file that you can include in IDAPython
