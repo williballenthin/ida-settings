d1afe85687306ca2418164dddf8607f52578a89d
Willi Ballenthin <willi.ballenthin@gmail.com>
2020-09-07 11:29:10 -0600

    setup: bump version to v2.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="ida-settings",
-    version="1.0.1",
+    version="2.0.1",
     description="Fetch and set configuration values in IDA Pro IDAPython scripts",
     author="Willi Ballenthin",
     author_email="william.ballenthin@fireeye.com",
