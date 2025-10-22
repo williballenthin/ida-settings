c0a4282f5a508a89b8784e524ba1511969b4b96b
Willi Ballenthin <willi.ballenthin@gmail.com>
2025-10-22 10:33:41 +0200

    add has/set/del/list current plugin settings

--- src/ida_settings/__init__.py
+++ src/ida_settings/__init__.py
@@ -1,1 +1,32 @@
-from hcli.lib.ida.plugin.settings import get_current_plugin_setting
+from hcli.lib.ida.plugin.settings import (
+    del_current_plugin_setting,
+    get_current_plugin_setting,
+    has_current_plugin_setting,
+    set_current_plugin_setting,
+)
+from hcli.lib.ida.plugin.settings import (
+    list_current_plugin_settings as _list_current_plugin_settings,
+)
+
+__all__ = [
+    "del_current_plugin_setting",
+    "get_current_plugin_setting",
+    "has_current_plugin_setting",
+    "set_current_plugin_setting",
+    "list_current_plugin_settings",
+]
+
+
+def list_current_plugin_settings() -> list[dict]:
+    descriptors = _list_current_plugin_settings()
+    return [
+        {
+            "key": d.key,
+            "type": d.type,
+            "default": d.default,
+            "required": d.required,
+            "documentation": d.documentation,
+            "choices": d.choices,
+        }
+        for d in descriptors
+    ]
